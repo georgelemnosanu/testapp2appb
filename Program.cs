f0c2da6ebe0759702059686bbb47121ad4dd8bbf
LMNDEV <georgelemnosanu@gmail.com>
2025-03-21 00:57:57 +0200

    test

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+app.Urls.Add("http://0.0.0.0:8080");
 
 app.MapGet("/deeplink", async (HttpContext context) =>
 {
